59c0b799ca8ce2f361cb2533b2bec0171bce1285
Str4tos <eschenkoden@gmail.com>
2016-09-06 15:08:50 +0300

    Removal of unnecessary
    Getting Started with github

---
+++ Assets/DropGravity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropGravity : MonoBehaviour {
+
+    private BoxCollider _BoxCollider;
+
+	// Use this for initialization
+	void Start () {
+        _BoxCollider = GetComponent<BoxCollider>();
+        
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "targetingPlane")
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+            _BoxCollider.isTrigger = false;
+        }
+    }
+}
