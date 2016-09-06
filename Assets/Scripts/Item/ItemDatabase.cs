7bc4331d1038108bc48a7e91f784bd89f9b37569
Str4tos <eschenkoden@gmail.com>
2016-09-06 18:52:52 +0300

    Repairing operability
    Backup before global change

--- Assets/Scripts/Item/ItemDatabase.cs
+++ Assets/Scripts/Item/ItemDatabase.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class ItemIngridients : Item {
+public class ItemDatabase : Item
+{
 
     public ItemType itemType = ItemType.Ingridients;
 
